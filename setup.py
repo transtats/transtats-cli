9732025680b713ee04df245578f7a03c0c8e8a08
sundeep-co-in <suanand@redhat.com>
2017-09-07 13:58:51 +0530

    package

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 """transtats command line tool"""
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='transtats-cli',
     version='0.1.0',
     description="Command line tool for transtats",
-    long_description="Command line tool for Transtats server",
     platforms=["Linux"],
-    py_modules=['transtats'],
+    packages=find_packages(),
     author="Sundeep Anand",
     author_email="suanand@redhat.com",
-    url="https://transtats.org",
+    url="http://transtats.org",
     license="Apache License 2.0",
     install_requires=[
         'Click', 'requests'
     ],
-    entry_points='''
-        [console_scripts]
-        transtats=cli:entry_point
-    ''',
+    scripts=["transtats"],
+    # entry_points='''
+    #     [console_scripts]
+    #     transtats=cli:entry_point
+    # ''',
     classifiers=[
         'License :: OSI Approved :: Apache License 2.0 (Apache-2.0)',
         'Environment :: Console',
